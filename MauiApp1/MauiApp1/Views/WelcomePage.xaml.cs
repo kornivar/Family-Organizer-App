616bd711dd828e4f153d9d66dea6b17bcb538c97
kornivar <kornivar62@gmail.com>
2026-09-26 19:29:40 +0300

    Add welcome page , change file structure

---
+++ MauiApp1/MauiApp1/Views/WelcomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1
+{
+    public partial class WelcomePage : ContentPage
+    {
+        int count = 0;
+
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGetStartedClicked(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Get Started button clicked");
+            //await Shell.Current.GoToAsync("LoginPage");
+        }
+    }
+}
